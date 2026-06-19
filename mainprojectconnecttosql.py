748934ee56720fa7417ee92062913c28f99eb24d
Fathima Safa <fathimasafaev22@gmail.com>
2026-06-19 16:46:40 +0530

    data cleaning
    The cleaned dataset is then connected to and stored in a MySQL database for efficient data management and analysis.

---
+++ mainprojectconnecttosql.py
@@ -0,0 +1,70 @@
+import mysql.connector
+import pandas as pd
+
+mydb = mysql.connector.connect(
+    host="127.0.0.1",
+    user="root",
+    password="Fathimaev@2003"
+)
+
+cursor = mydb.cursor()
+
+cursor.execute("CREATE DATABASE IF NOT EXISTS cleaned_appointments")
+
+mydb = mysql.connector.connect(
+    host="127.0.0.1",
+    user="root",
+    password="Fathimaev@2003",
+    database="cleaned_appointments"
+)
+
+cursor = mydb.cursor()
+
+df = pd.read_csv("cleaned_appointments.csv")
+
+df = df.rename(columns={
+    "hipertension": "hypertension",
+    "handcap": "handicap"
+})
+
+cols = [
+    "patientid", "gender", "age", "neighbourhood",
+    "hypertension", "diabetes", "alcoholism",
+    "handicap", "sms_received", "no_show", "waiting_days"
+]
+
+df = df[cols]
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS cleaned_appointments (
+    patientid BIGINT,
+    gender VARCHAR(10),
+    age INT,
+    neighbourhood VARCHAR(100),
+    hypertension INT,
+    diabetes INT,
+    alcoholism INT,
+    handicap INT,
+    sms_received INT,
+    no_show INT,
+    waiting_days INT
+)
+""")
+
+sql = """
+INSERT INTO cleaned_appointments (
+patientid, gender, age, neighbourhood,
+hypertension, diabetes, alcoholism,
+handicap, sms_received, no_show, waiting_days
+) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
+"""
+
+cursor.executemany(sql, df.values.tolist())
+
+mydb.commit()
+
+
+cursor.execute("SELECT * FROM cleaned_appointments LIMIT 5")
+
+for row in cursor:
+    print(row)
+
